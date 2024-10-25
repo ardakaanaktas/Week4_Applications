dec06e7935180a1057ccf6adb7e8853bc657e95b
Arda kaan AKTAŞ <arda_kaan_aktas@yahoo.com>
2024-10-25 04:55:01 +0300

    Encapsulation örneği yüklendi

---
+++ Encapsulation/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Car
+    {
+        //Fields
+        private string brand;
+        private string model;
+        private int doorCount;
+        private string color;
+
+        //Properties
+        public string Brand { get => brand; set => brand = value; }
+        public string Model { get => model; set => model = value; }
+        public int DoorCount { get => doorCount; set {
+                if (value == 2 || value == 4)//Araba kapı sayısı 2 veya 4 olabilir.
+                {
+                    doorCount = value;
+                }
+                else//Değilse hata mesajı ver ve kapı sayısını -1 yap.
+                {
+                    Console.WriteLine("Kapı sayısı 2 veya 4'ten farklı olamaz.");
+                    doorCount = -1;
+                }
+            } 
+        }
+        public string Color { get => color; set => color = value; }
+
+        //Methods
+        public void showCarInfo()
+        {
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Door Count: " + DoorCount);
+            Console.WriteLine("Color: " + Color);
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a Car object
+            Car car = new Car();
+
+
+            //Set the properties of the car object
+            car.Brand = "Ford";
+            car.Model = "Focus";
+            car.DoorCount = 3;
+            car.Color = "Red";
+
+            //Display the properties of the car object
+            car.showCarInfo();
+        }
+    }
+}
